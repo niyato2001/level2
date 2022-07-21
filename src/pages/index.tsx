ce93d7ea11baa6d415883c0ae4503a6f3264cb1c
niyato2001 <niyato2001@gmail.com>
2022-07-21 15:40:29 +0900

    feat: pages での PageMain の表示

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
 import { PageHeader } from '@/component/common/template/PageHeader';
+import { PageMain } from '@/component/common/template/PageMain';
 
 const Home: React.FC = () => (
   <main className='mx-auto w-[420px]'>
     <PageHeader />
+    <PageMain />
   </main>
 );
 
