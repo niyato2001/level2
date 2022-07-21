e650ecc2cd5450b3ba524a0cfc4374153af065b2
niyato2001 <niyato2001@gmail.com>
2022-07-21 14:33:43 +0900

    feat: pages の修正

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
+import { PageHeader } from '@/component/common/template/PageHeader';
+
 const Home: React.FC = () => (
-  <main className='mx-auto max-w-6xl py-6'>
-    <h1 className='text-lg font-bold text-primary-800'>
-      Welcome to Kikagaku Next.js Starter Kit!!
-    </h1>
+  <main className='mx-auto w-[420px]'>
+    <PageHeader />
   </main>
 );
 
