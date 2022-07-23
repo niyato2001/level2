9bd57b6547e686e5dd7ffbb83221caa6c7198d25
niyato2001 <niyato2001@gmail.com>
2022-07-23 15:17:26 +0900

    refactor: Card に state を作成

--- src/component/common/template/Card/Card.props.ts
+++ src/component/common/template/Card/Card.props.ts
@@ -4,13 +4,19 @@ import { CardProps } from './Card';
 export const toDoProps: CardProps = {
   type: 'TO DO',
   createLightIcon: createLightProps,
-  toDoList: ['テキスト', 'テキスト'],
+  toDoList: [
+    { title: 'テキスト1', isCompleted: false },
+    { title: 'テキスト2', isCompleted: false },
+  ],
 };
 
 export const completedProps: CardProps = {
   type: 'COMPLETED',
   createLightIcon: createLightProps,
-  toDoList: ['テキスト', 'テキスト'],
+  toDoList: [
+    { title: 'テキスト1', isCompleted: true },
+    { title: 'テキスト2', isCompleted: true },
+  ],
 };
 
 export const propObj: { [key: string]: CardProps } = {
--- src/component/common/template/Card/Card.tsx
+++ src/component/common/template/Card/Card.tsx
@@ -1,29 +1,51 @@
+import { useState } from 'react';
 import { Icon, IconProps } from '@/component/common/part/Icon';
+
+interface ToDoListProps {
+  title: string;
+  isCompleted: boolean;
+}
 
 export interface CardProps {
   type: 'TO DO' | 'COMPLETED';
   createLightIcon: IconProps;
-  toDoList: string[];
+  toDoList: ToDoListProps[];
 }
 
 export const baseId = 'common-template-card';
 
 export const Card: React.FC<CardProps> = ({ type, createLightIcon, toDoList }) => {
+  const initialToDos: ToDoListProps[] = [...toDoList];
+  const [toDos, setToDos] = useState<ToDoListProps[]>(initialToDos);
+  const handleClick = () => {
+    setToDos([...toDos, { title: '新規', isCompleted: false }]);
+    console.log(toDos);
+  };
+
   switch (type) {
     case 'TO DO':
       return (
         <div>
           <span className='tag bg-primary-700'>{type}</span>
           <div className='-mt-2 flex w-[360px] flex-col gap-y-2 bg-white px-8 py-4 shadow-md shadow-primary-200'>
-            <div className='text-right'>
-              <Icon {...createLightIcon} />
+            <div className='flex justify-end'>
+              <span className='justify-self-end' onClick={handleClick}>
+                <Icon {...createLightIcon} />
+              </span>
             </div>
-            {toDoList.map((toDo, i) => (
-              <div className='flex items-center gap-x-3' key={i}>
-                <span className='h-4 w-4 border-2 border-primary-700 bg-white'></span>
-                <span className='font-bold text-primary-700'>{toDo}</span>
-              </div>
-            ))}
+            {toDos
+              .filter((v) => v.isCompleted === false)
+              .map((toDo, i) => (
+                <div className='flex items-center gap-x-3' key={i}>
+                  <input
+                    type='checkbox'
+                    className='rounded-sm border-primary-700 text-primary-700 focus:ring-transparent'
+                    defaultChecked={toDo.isCompleted}
+                  />
+
+                  <span className='font-bold text-primary-700'>{toDo.title}</span>
+                </div>
+              ))}
           </div>
         </div>
       );
@@ -32,17 +54,23 @@ export const Card: React.FC<CardProps> = ({ type, createLightIcon, toDoList }) =
         <div>
           <span className='tag bg-pink-700'>{type}</span>
           <div className='-mt-2 flex w-[360px] flex-col gap-y-2 bg-white px-8 py-4 shadow-md shadow-primary-200'>
-            <div className='text-right'>
-              <Icon {...createLightIcon} />
+            <div className='flex justify-end'>
+              <span className='justify-self-end' onClick={handleClick}>
+                <Icon {...createLightIcon} />
+              </span>
             </div>
-            {toDoList.map((toDo, i) => (
-              <div className='flex items-center gap-x-3' key={i}>
-                <div className='flex h-4 w-4 items-center  bg-primary-700 text-white'>
-                  <span>✓</span>
+            {toDos
+              .filter((v) => v.isCompleted === true)
+              .map((toDo, i) => (
+                <div className='flex items-center gap-x-3' key={i}>
+                  <input
+                    type='checkbox'
+                    className='rounded-sm border-primary-700 text-primary-700 focus:ring-transparent'
+                    defaultChecked={toDo.isCompleted}
+                  />
+                  <span className='font-bold text-primary-700'>{toDo.title}</span>
                 </div>
-                <span className='font-bold text-primary-700'>{toDo}</span>
-              </div>
-            ))}
+              ))}
           </div>
         </div>
       );
