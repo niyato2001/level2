cb069fa320815ea1845bb0dabbb465016c04191c
niyato2001 <niyato2001@gmail.com>
2022-07-28 23:11:15 +0900

    feat: useState でModalの状態を追加

--- src/component/common/template/PageMain/PageMain.tsx
+++ src/component/common/template/PageMain/PageMain.tsx
@@ -10,17 +10,23 @@ export interface PageMainProps {
 
 export const baseId = 'common-template-page-main';
 
+const modals:string[]=['description',''];
+
 export const PageMain: React.FC<PageMainProps> = ({
   toDoCard,
   completedCard,
   descriptionModal,
 }) => {
-  const [modal, setModal] = useState(null);
+  const [modal, setModal] = useState(modals[-1]);
+  const handleClick = ():void => {
+    setModal('description')
+  };
   return (
     <div className='flex w-[420px] flex-col gap-y-5 bg-primary-50 p-8'>
       <span className='text-lg font-bold text-primary-700'>For what you wanna do!!</span>
       <Card {...toDoCard} />
       <Card {...completedCard} />
+      <button className='btn green-gradient' onClick={handleClick}>Green</button>
       {modal === 'description' && <DescriptionModal {...descriptionModal} />}
     </div>
   );
