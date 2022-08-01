4ae7a513b87b16b1c2721c606a2281f28edbde1c
niyato2001 <niyato2001@gmail.com>
2022-08-01 17:05:27 +0900

    feat: useModalを使用

--- src/component/common/template/PageMain/PageMain.tsx
+++ src/component/common/template/PageMain/PageMain.tsx
@@ -1,6 +1,6 @@
-import { useState } from 'react';
 import { Card, CardProps } from '../Card/Card';
 import { DescriptionModal, DescriptionModalProps } from '../DescriptionModal/DescriptionModal';
+import { useModal } from '@/hook/useModal';
 
 export interface PageMainProps {
   toDoCard: CardProps;
@@ -10,26 +10,23 @@ export interface PageMainProps {
 
 export const baseId = 'common-template-page-main';
 
-const modals: string[] = ['description', ''];
-
 export const PageMain: React.FC<PageMainProps> = ({
   toDoCard,
   completedCard,
   descriptionModal,
 }) => {
-  const [modal, setModal] = useState(modals[-1]);
-  const handleClick = (): void => {
-    setModal('description');
-  };
+  const { modal, descriptionClick, clearModal } = useModal();
   return (
     <div className='relative flex w-[420px] flex-col gap-y-5 bg-primary-50 p-8'>
       <span className='text-lg font-bold text-primary-700'>For what you wanna do!!</span>
       <Card {...toDoCard} />
       <Card {...completedCard} />
-      <button className='btn green-gradient' onClick={handleClick}>
+      <button className='btn green-gradient' onClick={descriptionClick}>
         Green
       </button>
-      {modal === 'description' && <DescriptionModal {...descriptionModal} />}
+      {modal === 'description' && (
+        <DescriptionModal {...descriptionModal} clearModal={clearModal} />
+      )}
     </div>
   );
 };
