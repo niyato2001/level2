db3988270900f6000292c87b6f556ea4ec65de0f
niyato2001 <niyato2001@gmail.com>
2022-08-01 17:04:28 +0900

    feat: useTodos を作成

---
+++ src/hook/useTodos.ts
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+
+export interface ToDoProps {
+  title: string;
+  isCompleted: boolean;
+  description: string;
+}
+
+interface UseTodosReturnType {
+  toDos: ToDoProps[];
+  handleClick: () => void;
+}
+
+const toDoList: ToDoProps[] = [
+  { title: 'テキスト1', isCompleted: false, description: 'テキスト1' },
+  { title: 'テキスト2', isCompleted: false, description: 'テキスト2' },
+];
+
+export const useTodos = (): UseTodosReturnType => {
+  const initialToDos = toDoList;
+  const [toDos, setToDos] = useState<ToDoProps[]>(initialToDos);
+  const handleClick = () => {
+    setToDos([...toDos, { title: '新規', isCompleted: false, description: '新規' }]);
+  };
+  return { toDos, handleClick };
+};
