591b41bee9794836652703a6b8ace1151d7e75a1
niyato2001 <niyato2001@gmail.com>
2022-08-01 22:33:42 +0900

    feat: defaultProps の export を間接的に

--- src/component/common/template/DescriptionModal/DescriptionModal.props.ts
+++ src/component/common/template/DescriptionModal/DescriptionModal.props.ts
@@ -1,7 +1,7 @@
 import { deleteDeepProps } from '../../part/Icon/Icon.props';
 import { DescriptionModalProps } from './DescriptionModal';
 
-export const defaultProps: DescriptionModalProps = {
+const defaultProps: DescriptionModalProps = {
   deleteDeepIcon: deleteDeepProps,
   title: '経営の必読書を読む',
   description: ['イシューから始めよ', '起業のファイナンス', '起業の科学'],
