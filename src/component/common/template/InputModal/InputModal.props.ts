5d9633e494e8791ae678ccee935f20977fd82942
niyato2001 <niyato2001@gmail.com>
2022-08-02 16:31:52 +0900

    feat: InputModalの入力データ情報を削除

--- src/component/common/template/InputModal/InputModal.props.ts
+++ src/component/common/template/InputModal/InputModal.props.ts
@@ -4,15 +4,11 @@ import { InputModalProps } from './InputModal';
 const defaultProps: InputModalProps = {
   type: 'create',
   deleteDeepIcon: deleteDeepProps,
-  title: 'タイトル',
-  description: '内容',
 };
 
 const updateProps: InputModalProps = {
   type: 'update',
   deleteDeepIcon: deleteDeepProps,
-  title: 'タイトル',
-  description: '内容',
 };
 
 export const propObj: { [key: string]: InputModalProps } = {
