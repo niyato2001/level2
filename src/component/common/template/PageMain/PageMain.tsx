87ce88bc1d8a0525dcdd906f6cd55873be10d84d
niyato2001 <niyato2001@gmail.com>
2022-08-04 09:51:29 +0900

    refactor: PageMain の型を整理

--- src/component/common/template/PageMain/PageMain.tsx
+++ src/component/common/template/PageMain/PageMain.tsx
@@ -1,51 +1,77 @@
-import { Card, CardFcProps } from '../Card/Card';
+import { Dispatch, SetStateAction } from 'react';
+import { Card, CardProps } from '../Card/Card';
 import { DescriptionModal, DescriptionModalProps } from '../DescriptionModal/DescriptionModal';
-import { InputModal, InputModalProps } from '../InputModal/InputModal';
-import { useModal } from '@/hook/useModal';
-import { useTodos } from '@/hook/useTodos';
+import { InputModal } from '../InputModal';
+import { InputModalDataProps, InputModalLogic2Props } from '../InputModal/InputModal.type';
+import { ToDoProps } from '@/hook/useTodos';
 
 export interface PageMainProps {
-  toDoCard: CardFcProps;
-  completedCard: CardFcProps;
+  toDoCard: CardProps;
+  completedCard: CardProps;
   descriptionModal: DescriptionModalProps;
-  createModal: InputModalProps;
-  updateModal: InputModalProps;
+  createModal: InputModalDataProps;
+  updateModal: InputModalDataProps;
+  logicProps: InputModalLogic2Props;
+}
+
+export interface PageMainFcProps extends PageMainProps {
+  toDos: ToDoProps[];
+  createClick: () => void;
+  descriptionClick: () => void;
+  updateClick: () => void;
+  clearModal: () => void;
+  setToDos: Dispatch<SetStateAction<ToDoProps[]>>;
+  modal: string;
 }
 
 export const baseId = 'common-template-page-main';
 
-export const PageMain: React.FC<PageMainProps> = ({
+export const PageMain: React.FC<PageMainFcProps> = ({
   toDoCard,
   completedCard,
   descriptionModal,
   createModal,
   updateModal,
-}) => {
-  const { modal, descriptionClick, clearModal, createClick, updateClick } = useModal();
-  const { toDos, setToDos } = useTodos();
-  return (
-    <div className='relative flex w-[420px] flex-col gap-y-5 bg-primary-50 p-8'>
-      <span className='text-lg font-bold text-primary-700'>For what you wanna do!!</span>
-      <Card {...toDoCard} toDos={toDos} handleClick={createClick} />
-      <Card {...completedCard} toDos={toDos} handleClick={createClick} />
-      <button className='btn green-gradient' onClick={descriptionClick}>
-        description
-      </button>
-      <button className='btn green-gradient' onClick={createClick}>
-        create
-      </button>
-      <button className='btn green-gradient' onClick={updateClick}>
-        update
-      </button>
-      {modal === 'description' && (
-        <DescriptionModal {...descriptionModal} clearModal={clearModal} />
-      )}
-      {modal === 'create' && (
-        <InputModal {...createModal} clearModal={clearModal} toDos={toDos} setToDos={setToDos} />
-      )}
-      {modal === 'update' && (
-        <InputModal {...updateModal} clearModal={clearModal} toDos={toDos} setToDos={setToDos} />
-      )}
-    </div>
-  );
-};
+  logicProps,
+  toDos,
+  createClick,
+  descriptionClick,
+  updateClick,
+  clearModal,
+  setToDos,
+  modal,
+}) => (
+  <div className='relative flex w-[420px] flex-col gap-y-5 bg-primary-50 p-8'>
+    <span className='text-lg font-bold text-primary-700'>For what you wanna do!!</span>
+    <Card {...toDoCard} toDos={toDos} handleClick={createClick} />
+    <Card {...completedCard} toDos={toDos} handleClick={createClick} />
+    <button className='btn green-gradient' onClick={descriptionClick}>
+      description
+    </button>
+    <button className='btn green-gradient' onClick={createClick}>
+      create
+    </button>
+    <button className='btn green-gradient' onClick={updateClick}>
+      update
+    </button>
+    {modal === 'description' && <DescriptionModal {...descriptionModal} clearModal={clearModal} />}
+    {modal === 'create' && (
+      <InputModal
+        {...createModal}
+        {...logicProps}
+        clearModal={clearModal}
+        toDos={toDos}
+        setToDos={setToDos}
+      />
+    )}
+    {modal === 'update' && (
+      <InputModal
+        {...updateModal}
+        {...logicProps}
+        clearModal={clearModal}
+        toDos={toDos}
+        setToDos={setToDos}
+      />
+    )}
+  </div>
+);
--- src/component/common/template/PageMain/index.tsx
+++ src/component/common/template/PageMain/index.tsx
@@ -1,6 +1,8 @@
+import { Dispatch, SetStateAction } from 'react';
 import { PageMain as PageMainPresenter, PageMainProps } from './PageMain';
 import { propObj } from './PageMain.props';
-
+import { useModal } from '@/hook/useModal';
+import { ToDoProps, useTodos } from '@/hook/useTodos';
 /**
  * ロジックが存在しない（= Container が要らない）場合は 以下と置き換えてください。
  * 存在する場合はコメントアウト部分を全て削除して使ってください。
@@ -10,9 +12,31 @@ export type { PageMainProps };
 export { PageMainPresenter};
 */
 
+type PageMainContainerProps = {
+  modal: string;
+  toDos: ToDoProps[];
+  setToDos: Dispatch<SetStateAction<ToDoProps[]>>;
+  clearModal: () => void;
+  descriptionClick: () => void;
+  createClick: () => void;
+  updateClick: () => void;
+};
+
 const PageMain: React.FC = () => {
+  const { modal, descriptionClick, clearModal, createClick, updateClick } = useModal();
+  const { toDos, setToDos } = useTodos();
+  const logicProps: PageMainContainerProps = {
+    modal: modal,
+    toDos: toDos,
+    descriptionClick: descriptionClick,
+    clearModal: clearModal,
+    createClick: createClick,
+    updateClick: updateClick,
+    setToDos: setToDos,
+  };
+
   const defaultProps: PageMainProps = { ...propObj.default };
-  return <PageMainPresenter {...defaultProps} />;
+  return <PageMainPresenter {...defaultProps} {...logicProps} />;
 };
 
 export { PageMain };
