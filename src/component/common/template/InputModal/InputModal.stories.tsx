0e76784a47284299b943f905db7430f01a73ae23
niyato2001 <niyato2001@gmail.com>
2022-08-04 10:00:57 +0900

    refactor: stories の型を修正

--- src/component/common/template/InputModal/InputModal.stories.tsx
+++ src/component/common/template/InputModal/InputModal.stories.tsx
@@ -1,13 +1,14 @@
 import type { ComponentMeta, Story } from '@storybook/react';
-import { InputModal, InputModalFcProps } from './InputModal';
+import { InputModal } from './InputModal';
 import { propObj } from './InputModal.props';
+import { InputModalPresenterProps } from './InputModal.type';
 
 export default {
   title: 'Common/template/InputModal',
   component: InputModal,
 } as ComponentMeta<typeof InputModal>;
 
-const Template: Story<InputModalFcProps> = (args) => <InputModal {...args} />;
+const Template: Story<InputModalPresenterProps> = (args) => <InputModal {...args} />;
 
 export const Default = Template.bind({});
 Default.args = propObj.default;
