a6b474b9fd8d1e8e9641753a75f5298760822b8f
niyato2001 <niyato2001@gmail.com>
2022-08-04 10:47:00 +0900

    refactor: PageMain.stories を修正

--- src/component/common/template/PageMain/PageMain.stories.tsx
+++ src/component/common/template/PageMain/PageMain.stories.tsx
@@ -1,13 +1,14 @@
 import type { ComponentMeta, Story } from '@storybook/react';
-import { PageMain, PageMainProps } from './PageMain';
+import { PageMain } from './PageMain';
 import { propObj } from './PageMain.props';
+import { PageMainPresenterProps } from './PageMain.type';
 
 export default {
   title: 'Common/template/PageMain',
   component: PageMain,
 } as ComponentMeta<typeof PageMain>;
 
-const Template: Story<PageMainProps> = (args) => <PageMain {...args} />;
+const Template: Story<PageMainPresenterProps> = (args) => <PageMain {...args} />;
 
 export const Default = Template.bind({});
 Default.args = propObj.default;
