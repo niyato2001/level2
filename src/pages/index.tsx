04dc386528b21e6970d333252f4006515aa8137b
niyato2001 <niyato2001@gmail.com>
2022-08-08 16:50:11 +0900

    feat: index.tsx から API に POST

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,39 @@
-import type { GetServerSideProps, NextPage } from 'next';
+import axios from 'axios';
+import type { GetStaticProps, NextPage } from 'next';
 import { PageHeader } from '@/component/common/template/PageHeader';
 import { PageMain } from '@/component/common/template/PageMain';
 import { ToDoProps } from '@/hook/useTodos';
 
 import { prisma } from '@/lib/prisma';
 
-const Home: NextPage<Props> = ({ count, todos }) => (
-  <main className='mx-auto w-[420px]'>
-    <PageHeader />
-    <PageMain />
-    {`現在のtodoの数は${count}です`}
-    <div> 最後のtodoのタイトルは{todos[2].description}です</div>
-  </main>
-);
+const Home: NextPage<Props> = ({ count, todos }) => {
+  const addHome = (): Promise<ToDoProps> =>
+    axios.post('/api/create-todo', {
+      title: '難しい',
+      description: '超難しい',
+      isCompleted: false,
+      id: 400,
+    });
+  console.log(todos);
+  return (
+    <main className='mx-auto w-[420px]'>
+      <PageHeader />
+      <PageMain />
+      {`現在のtodoの数は${count}です`}
+      <div> 最後のtodoのタイトルは{todos[2].description}です</div>
+      <button onClick={addHome} className='btn green-gradient'>
+        追加
+      </button>
+    </main>
+  );
+};
 
 interface Props {
   count: number;
   todos: ToDoProps[];
 }
 
-export const getServerSideProps: GetServerSideProps<Props> = async () => {
+export const getStaticProps: GetStaticProps<Props> = async () => {
   const count = await prisma.todo.count();
   const todos = await prisma.todo.findMany();
   return {
@@ -27,6 +41,7 @@ export const getServerSideProps: GetServerSideProps<Props> = async () => {
       count,
       todos: JSON.parse(JSON.stringify(todos)),
     },
+    revalidate: 30,
   };
 };
 
